b8b5c2cf607d8173849c67aac9b8e380e550e795
jules-party <83989081+jules-party@users.noreply.github.com>
2023-05-23 23:01:06 -0400

    Make commands usuable

--- src/main/java/zoink/jule/waypoints/Waypoints.java
+++ src/main/java/zoink/jule/waypoints/Waypoints.java
@@ -2,12 +2,29 @@ package zoink.jule.waypoints;
 
 import java.io.File;
 import org.bukkit.plugin.java.JavaPlugin;
+import zoink.jule.waypoints.Commands.WDelete;
+import zoink.jule.waypoints.Commands.WList;
+import zoink.jule.waypoints.Commands.WSave;
+import zoink.jule.waypoints.Commands.WTp;
+import zoink.jule.waypoints.Utils.Completer;
 
 public final class Waypoints extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        super.onEnable();
+        System.out.println("Waypoints Enabled!");
 
+        createWaypointsDir();
+        this.getCommand("wsave").setExecutor(new WSave());
+
+        this.getCommand("wtp").setExecutor(new WTp());
+        this.getCommand("wtp").setTabCompleter(new Completer());
+
+        this.getCommand("wlist").setExecutor(new WList());
+
+        this.getCommand("wdelete").setExecutor(new WDelete());
+        this.getCommand("wdelete").setTabCompleter(new Completer());
     }
 
     private void createWaypointsDir() {
@@ -15,5 +32,11 @@ public final class Waypoints extends JavaPlugin {
 
         if (!dir.exists() || !dir.isDirectory())
             dir.mkdirs();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        System.out.println("Plugin Disabled!");
     }
 }
